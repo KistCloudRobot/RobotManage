761b2f444fbe69ccc03ba3141623947a5d8873f3
Ron <mhkim@i-on.net>
2022-12-06 10:24:16 +0900

    ..

--- cloud-server/src/main/java/net/ion/cloudrobot/controller/RootController.java
+++ cloud-server/src/main/java/net/ion/cloudrobot/controller/RootController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RootController implements ErrorController {
-
     @GetMapping("/error")
     public String redirectRoot() {
         return "index.html";
--- cloud-server/src/main/java/net/ion/cloudrobot/model/enums/RobotType.java
+++ cloud-server/src/main/java/net/ion/cloudrobot/model/enums/RobotType.java
@@ -1,5 +1,5 @@
 package net.ion.cloudrobot.model.enums;
 
 public enum RobotType {
-    COBOT, ARM_TOW, PALLETIZER, AMR_LIFT, TEMP
+    COBOT, ARM_TOW, PALLETIZER, AMR_LIFT
 }
