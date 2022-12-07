aac89311ee213d4ebfed4426a587af906c5f9f5b
Ron <mhkim@i-on.net>
2022-12-07 16:07:08 +0900

    Add Javadoc User Repository

--- cloud-server/src/main/java/net/ion/mdk/auth/repository/UserRepository.java
+++ cloud-server/src/main/java/net/ion/mdk/auth/repository/UserRepository.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * User Account JPA Repository
+ */
 @Repository
 public interface UserRepository extends JpaRepository<Account, Long> {
 
