08b42336283005ebafd5a776a7bac7720580904a
Ron <mhkim@i-on.net>
2022-12-07 16:35:00 +0900

    Add Javadoc

--- cloud-server/src/main/java/net/ion/mdk/config/SwaggerConfig.java
+++ cloud-server/src/main/java/net/ion/mdk/config/SwaggerConfig.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
+    /**
+     * @return GroupedOpenApi
+     */
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
